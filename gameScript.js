f7397be76b6fe25208a4538d26ee23548b930a8f
kisbayu <kisbayu.k.a@gmail.com>
2021-12-07 21:34:46 +0700

    done

---
+++ gameScript.js
@@ -0,0 +1,137 @@
+//player object
+function Player(){
+    this.Choose = ""
+    this.Play = function(){
+        if (this.Choose=="batu" && computer.Choose=="batu")this.changeStatus("draw");
+        if (this.Choose=="batu" && computer.Choose=="gunting")this.changeStatus("win");
+        if (this.Choose=="batu" && computer.Choose=="kertas")this.changeStatus("lose");
+        if (this.Choose=="kertas" && computer.Choose=="batu")this.changeStatus("win");
+        if (this.Choose=="kertas" && computer.Choose=="gunting")this.changeStatus("lose");
+        if (this.Choose=="kertas" && computer.Choose=="kertas")this.changeStatus("draw");
+        if (this.Choose=="gunting" && computer.Choose=="batu")this.changeStatus("lose");
+        if (this.Choose=="gunting" && computer.Choose=="gunting")this.changeStatus("draw");
+        if (this.Choose=="gunting" && computer.Choose=="kertas")this.changeStatus("win");
+        if (this.Choose=="" && computer.Choose=="")this.changeStatus("");
+    }
+    this.changeBG = function(){
+        if(this.Choose== "batu"){
+            player_batu.style.backgroundColor = "rgb(196, 196, 196)";
+            player_kertas.style.backgroundColor = "rgb(156, 131, 95)";
+            player_gunting.style.backgroundColor = "rgb(156, 131, 95)";
+        }
+        if(this.Choose == "gunting"){
+            player_batu.style.backgroundColor = "rgb(156, 131, 95)";
+            player_kertas.style.backgroundColor = "rgb(156, 131, 95)";
+            player_gunting.style.backgroundColor = "rgb(196, 196, 196)";
+        }
+        if(this.Choose == "kertas"){
+            player_batu.style.backgroundColor = "rgb(156, 131, 95)";
+            player_kertas.style.backgroundColor = "rgb(196, 196, 196)";
+            player_gunting.style.backgroundColor = "rgb(156, 131, 95)";
+        }
+        if(this.Choose == ""){
+            player_batu.style.backgroundColor = "rgb(156, 131, 95)";
+            player_kertas.style.backgroundColor = "rgb(156, 131, 95)";
+            player_gunting.style.backgroundColor = "rgb(156, 131, 95)";
+        }
+    }
+    this.changeStatus = function(status){
+        if (status == "win"){
+            stat.innerHTML = "PLAYER 1 <br> WIN";
+            stat.classList.add('rectangle');
+            stat.style.backgroundColor = "rgb(76, 150, 84)";
+        }
+        if (status == "lose") {
+            stat.innerHTML = "COM <br> WIN"
+            stat.classList.add('rectangle');
+            stat.style.backgroundColor = "rgb(76, 150, 84)";
+        }
+        if (status == "draw") {
+            stat.innerHTML = "DRAW";
+            stat.classList.add('rectangle');
+            stat.style.backgroundColor = "rgb(3, 91, 12)";
+        }
+        if (status == "") {
+            stat.innerHTML = "VS";
+            stat.classList.remove('rectangle');
+            stat.style.backgroundColor = "rgb(156, 131, 95)";
+        }
+    }
+}
+
+//computer object
+function Computer(){
+    this.Choose = ""
+    this.changeBG = function(){
+        if(this.Choose== "batu"){
+            com_batu.style.backgroundColor = "rgb(196, 196, 196)";
+            com_kertas.style.backgroundColor = "rgb(156, 131, 95)";
+            com_gunting.style.backgroundColor = "rgb(156, 131, 95)";
+        }
+        if(this.Choose == "gunting"){
+            com_batu.style.backgroundColor = "rgb(156, 131, 95)";
+            com_kertas.style.backgroundColor = "rgb(156, 131, 95)";
+            com_gunting.style.backgroundColor = "rgb(196, 196, 196)";
+        }
+        if(this.Choose == "kertas"){
+            com_batu.style.backgroundColor = "rgb(156, 131, 95)";
+            com_kertas.style.backgroundColor = "rgb(196, 196, 196)";
+            com_gunting.style.backgroundColor = "rgb(156, 131, 95)";
+        }
+        if(this.Choose == ""){
+            com_batu.style.backgroundColor = "rgb(156, 131, 95)";
+            com_kertas.style.backgroundColor = "rgb(156, 131, 95)";
+            com_gunting.style.backgroundColor = "rgb(156, 131, 95)";
+        }
+    }
+    this.randomPick = function(){
+        let random = Math.floor(Math.random() * 3);
+        if (random == 0) return "batu";
+        if (random == 1) return "gunting";
+        if (random == 2) return "kertas";
+    }
+}
+
+let player1 = new Player();
+let computer = new Computer();
+
+document.getElementById("player_batu").onclick = function(){
+    player1.Choose = "batu";
+    player1.changeBG();
+    computer.Choose = computer.randomPick();
+    player1.Play();
+    computer.changeBG();
+    console.log("player 1 = "+player1.Choose);
+    console.log("computer = "+computer.Choose)
+}
+
+document.getElementById("player_kertas").onclick = function(){
+    player1.Choose = "kertas";
+    player1.changeBG();
+    computer.Choose = computer.randomPick();
+    player1.Play();
+    computer.changeBG();
+    console.log("player 1 = "+player1.Choose);
+    console.log("computer = "+computer.Choose)
+}
+
+document.getElementById("player_gunting").onclick = function(){
+    player1.Choose = "gunting";
+    player1.changeBG();
+    computer.Choose = computer.randomPick();
+    player1.Play();
+    computer.changeBG();
+    console.log("player 1 = "+player1.Choose);
+    console.log("computer = "+computer.Choose)
+}
+
+document.getElementById("refresh").onclick = function(){
+    player1.Choose = "";
+    player1.changeBG();
+    computer.Choose = "";   
+    computer.changeBG();
+    player1.Play();
+    console.log("refresh");
+    console.log("player 1 = "+player1.Choose);
+    console.log("computer = "+computer.Choose)
+}
